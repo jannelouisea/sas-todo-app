e0960e981dddf609ff0a0d925c992763444574c5
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-25 01:31:47 -0400

    Create base for adding a todo list item

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -6,12 +6,25 @@ import { todoItemAdded } from 'src/features/todoItems/todoItemsSlice'
 import {
     IToDoItem
 } from 'src/interfaces/'
+import moment from 'moment'
 
 function ToDoList() {
     const todoItems = useAppSelector(state => state.todoItems);
+    const dispatch = useAppDispatch();
+
+    const handleClick = () => {
+        dispatch(todoItemAdded({
+            id: `todo-item-${todoItems.length + 1}`,
+            text: `Hello World Again`,
+            createdAt: moment().toDate(),
+        }))
+    };
 
     return (
-        <>
+        <div className="m-6">
+            <div>
+                <button onClick={handleClick}>Add Item</button>
+            </div>
             {
                 todoItems.map((item: IToDoItem) =>
                     <div key={item.id} className="mb-1">
@@ -25,7 +38,7 @@ function ToDoList() {
             <Item />
             <Item />
             <Item />
-        </>
+        </div>
     );
 }
 
