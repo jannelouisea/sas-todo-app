3299060fd29f467f144915b250a896b6b4992af3
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-24 23:56:10 -0400

    Creating todoItemsSlice

---
+++ src/redux/todoStore/slice.test.ts
@@ -0,0 +1,137 @@
+import {
+    IToDoItem
+} from 'src/interfaces'
+import moment from 'moment';
+import reducer, { todoItemAdded, todoItemToggled } from './slice'
+
+describe('todoSlice', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, { type: undefined })).toEqual([]);
+    });
+
+    it('should handle an item added to an new list', () => {
+        const previousState: IToDoItem[] = [];
+
+        const id = 'todo-item-001';
+        const text = 'Clean dishes';
+        const createdAt = moment().toDate();
+
+        const newState: IToDoItem[] = [{
+            id,
+            text,
+            createdAt,
+            completed: false,
+        }];
+
+        expect(reducer(previousState, todoItemAdded({ id, text, createdAt }))).toEqual(newState);
+    });
+
+    it('should handle an item added to a existing list', () => {
+        const createdAt01 = moment().toDate();
+        const previousState: IToDoItem[] = [
+            {
+                id: 'todo-item-001',
+                text: 'Walk the dog',
+                createdAt: createdAt01,
+                completed: true,
+            }
+        ];
+
+        const id = 'todo-item-002';
+        const text = 'Clean dishes';
+        const createdAt = moment().toDate();
+
+        const newState: IToDoItem[] = [
+            {
+                id: 'todo-item-001',
+                text: 'Walk the dog',
+                createdAt: createdAt01,
+                completed: true,
+            },
+            {
+                id,
+                text,
+                createdAt,
+                completed: false,
+            }
+        ];
+
+        expect(reducer(previousState, todoItemAdded({ id, text, createdAt }))).toEqual(newState);
+    });
+
+    it('should handle an item toggled completed to true', () => {
+        const id = 'todo-item-001';
+        const createdAt = moment().toDate();
+        const previousState: IToDoItem[] = [
+            {
+                id,
+                text: 'Walk the dog',
+                createdAt,
+                completed: false,
+            }
+        ];
+        const newState: IToDoItem[] = [
+            {
+                id,
+                text: 'Walk the dog',
+                createdAt,
+                completed: true,
+            }
+        ];
+
+        expect(reducer(previousState, todoItemToggled(id))).toEqual(newState);
+    });
+
+    it('should handle an item toggled completed to false', () => {
+        const id = 'todo-item-001';
+        const createdAt = moment().toDate();
+        const previousState: IToDoItem[] = [
+            {
+                id,
+                text: 'Walk the dog',
+                createdAt,
+                completed: true,
+            }
+        ];
+        const newState: IToDoItem[] = [
+            {
+                id,
+                text: 'Walk the dog',
+                createdAt,
+                completed: false,
+            }
+        ];
+
+        expect(reducer(previousState, todoItemToggled(id))).toEqual(newState);
+    });
+
+    it('should handle unchanged todo list when id doesn\'t exist', () => {
+        const state: IToDoItem[] = [
+            {
+                id: 'todo-item-001',
+                text: 'Walk the dog',
+                createdAt: moment().toDate(),
+                completed: true,
+            }
+        ];
+
+        expect(reducer(state, todoItemToggled('does-not-exist'))).toEqual(state);
+    });
+
+    it('should handle unchanged todo list when id is an empty string', () => {
+        const state: IToDoItem[] = [
+            {
+                id: 'todo-item-001',
+                text: 'Walk the dog',
+                createdAt: moment().toDate(),
+                completed: true,
+            }
+        ];
+
+        expect(reducer(state, todoItemToggled(''))).toEqual(state);
+    });
+});
+
+test('should return the initial state', () => {
+    expect(reducer(undefined, { type: undefined })).toEqual([]);
+});
---
+++ src/redux/todoStore/slice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import {
+    IToDoItem
+} from 'src/interfaces'
+
+export type ITodoItemAddedPayload = {
+    id: string,
+    text: string,
+    createdAt: Date,
+};
+
+const initialState: IToDoItem[] = [];
+
+const todoItemsSlice = createSlice({
+    name: 'todoItems',
+    initialState,
+    reducers: {
+        todoItemAdded(state, action: PayloadAction<ITodoItemAddedPayload>) {
+            const { id, text, createdAt } = action.payload;
+            state.push({
+                id,
+                text,
+                createdAt,
+                completed: false
+            });
+        },
+        // action - id of the item being toggled
+        todoItemToggled(state, action: PayloadAction<string>) {
+            const todoItem = state.find(item => item.id === action.payload);
+            if (todoItem) {
+                todoItem.completed = !todoItem.completed
+            }
+        }
+    }
+});
+
+export const { todoItemAdded, todoItemToggled } = todoItemsSlice.actions;
+
+export default todoItemsSlice.reducer;
