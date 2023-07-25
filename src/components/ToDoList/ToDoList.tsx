b58846dbd53b5453c17359ec587209307224ebb3
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-25 01:19:35 -0400

    Using todoItems from Redux store

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,10 +1,26 @@
 import {
     Item
 } from 'src/components'
+import { useAppDispatch, useAppSelector } from 'src/app/hooks'
+import { todoItemAdded } from 'src/features/todoItems/todoItemsSlice'
+import {
+    IToDoItem
+} from 'src/interfaces/'
 
 function ToDoList() {
+    const todoItems = useAppSelector(state => state.todoItems);
+
     return (
         <>
+            {
+                todoItems.map((item: IToDoItem) =>
+                    <div key={item.id} className="mb-1">
+                        <div>{item.text}</div>
+                        <div>{item.createdAt.toDateString()}</div>
+                        <div>{`${item.completed}`}</div>
+                    </div>
+                )
+            }
             <Item />
             <Item />
             <Item />
