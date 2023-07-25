06d9916eaf4a50f15adb8f57d1a84fe73d568c1e
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-25 18:07:00 -0400

    Cleaning up experimental ToDoList

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,24 +1,12 @@
-import {
-    Item
-} from 'src/components'
 import { useAppDispatch, useAppSelector } from 'src/app/hooks'
-import { todoItemAdded, todoItemToggled } from 'src/features/todoItems/todoItemsSlice'
+import { todoItemToggled } from 'src/features/todoItems/todoItemsSlice'
 import {
     IToDoItem
 } from 'src/interfaces/'
-import moment from 'moment'
 
 function ToDoList() {
     const todoItems = useAppSelector(state => state.todoItems);
     const dispatch = useAppDispatch();
-
-    const handleAddItem = () => {
-        dispatch(todoItemAdded({
-            id: `todo-item-${todoItems.length + 1}`,
-            text: `Hello World Again`,
-            createdAt: moment().toDate(),
-        }))
-    };
 
     const handleToggleItem = (id: string) => {
         dispatch(todoItemToggled(id));
@@ -26,9 +14,6 @@ function ToDoList() {
 
     return (
         <div className="m-6">
-            <div>
-                <button onClick={handleAddItem}>Add Item</button>
-            </div>
             {
                 todoItems.map((item: IToDoItem) =>
                     <div key={item.id} className="mb-1">
