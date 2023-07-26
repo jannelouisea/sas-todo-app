dcaffed763d9ccc856759b655fd6c34321558ac8
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-26 11:47:55 -0400

    Checking for empty to do item text

--- src/components/AddItemFieldBar/AddItemFieldBar.tsx
+++ src/components/AddItemFieldBar/AddItemFieldBar.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import { useAppDispatch } from 'src/app/hooks';
 import { todoItemAdded } from 'src/features/todoItems/todoItemsSlice';
 
@@ -7,38 +6,66 @@ import moment from 'moment';
 import uniqid from 'uniqid';
 
 import Input from '@mui/joy/Input';
+import Typography from '@mui/joy/Typography';
 import AddItemButton from './AddItemButton';
 
 function AddItemFieldBar() {
 
     const INITIAL_TEXT_STATE = '';
+    const IS_TEXT_EMPTY_ERR_MSG = 'To-do item cannot be empty.'
     const dispatch = useAppDispatch();
 
     // TODO - Additions
     // Adding a number count?
-    // Check for empty string
     const [text, setText] = useState(INITIAL_TEXT_STATE);
+    const [textChanged, setTextChanged] = useState(false);
+    const [hasError, setHasError] = useState(false);
+
+    useEffect(() => {
+        const isEmpty = textChanged && text === '';
+        setHasError(isEmpty);
+    }, [text, textChanged]);
+
+    const onTextChanged = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setTextChanged(true);
+        setText(evt.target.value);
+    }
 
     const onAddItem = () => {
-        dispatch(todoItemAdded({
-            id: uniqid(),
-            text,
-            createdAt: moment().toDate(),
-        }));
+        const isEmpty = text === '';
 
+        if (!isEmpty) {
+            dispatch(todoItemAdded({
+                id: uniqid(),
+                text,
+                createdAt: moment().toDate(),
+            }));
+
+            resetState();
+        } else {
+            setHasError(true);
+        }
+    };
+
+    const resetState = () => {
+        setTextChanged(false);
         setText(INITIAL_TEXT_STATE);
     };
 
     return (
         <div className='flex'>
-            <Input
-                className='flex-1 drop-shadow-xl'
-                placeholder='Add a to-do item...'
-                size='lg'
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                sx={{ borderRadius: '4rem' }}
-            />
+            <div className='flex-1'>
+                <Input
+                    className='drop-shadow-xl'
+                    placeholder='Add a to-do item...'
+                    size='lg'
+                    value={text}
+                    onChange={onTextChanged}
+                    sx={{ borderRadius: '4rem' }}
+                    error={hasError}
+                />
+                {hasError && <Typography color='danger' level='body3' variant='plain' sx={{ marginTop: '.25rem', paddingLeft: '1.5rem' }}>{IS_TEXT_EMPTY_ERR_MSG}</Typography>}
+            </div>
             <div className='ml-2'>
                 <AddItemButton onClick={onAddItem} />
             </div>
