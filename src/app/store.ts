69afe9892d89bf805300621a7ffd44af0de35f3c
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-26 10:54:08 -0400

    Adding todoItemsFiltersReducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import todoItemsReducer from 'src/features/todoItems/todoItemsSlice'
+import todoItemsFiltersReducer from 'src/features/todoItemsFilters/todoItemsFiltersSlice'
 
 export const store = configureStore({
     reducer: {
         todoItems: todoItemsReducer,
+        todoItemsFilters: todoItemsFiltersReducer,
     }
 });
 
