94aa4d1fd1d8a8dc15e43caf4bd4acc369345cb4
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-26 10:54:42 -0400

    Adding searching capability to ToDoList

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,19 +1,30 @@
 import { useAppSelector } from 'src/app/hooks'
 import {
     IToDoItem
-} from 'src/interfaces/'
+} from 'src/interfaces'
 import {
     Item
 } from 'src/components'
 
 function ToDoList() {
     const todoItems = useAppSelector(state => state.todoItems);
+    const todoItemsFilters = useAppSelector(state => state.todoItemsFilters);
+
+    const itemMeetsCriteria = (item: IToDoItem) => {
+        let meetsCriteria: boolean = true;
+
+        if (todoItemsFilters.searchTerm) {
+            meetsCriteria = meetsCriteria && item.text.includes(todoItemsFilters.searchTerm);
+        }
+
+        return meetsCriteria;
+    }
 
     return (
         // JLA - Work on what happens with multiple elements
         <div className="grid grid-flow-row row-span-1 overflow-auto">
             {
-                todoItems.map((item: IToDoItem) => <div className='mb-4'><Item item={item} /></div>)
+                todoItems.map((item: IToDoItem) => itemMeetsCriteria(item) ? <div key={item.id} className='mb-4'><Item item={item} /></div> : <></>)
             }
         </div>
     );
