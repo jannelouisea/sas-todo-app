a045a5c3af4a5f1536e1bf925915c1f35c807918
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-25 23:14:41 -0400

    Adding dummy search bar to the top

--- src/components/ToDoApp/ToDoApp.tsx
+++ src/components/ToDoApp/ToDoApp.tsx
@@ -2,11 +2,27 @@ import {
     AddItemFieldBar,
     ToDoList
 } from 'src/components';
+import Input from '@mui/joy/Input';
+import Divider from '@mui/joy/Divider';
+import SearchIcon from '@mui/icons-material/Search';
 
 function ToDoApp() {
     return (
         <div className="w-screen h-screen flex justify-center items-center">
             <div className="w-96 lg:w-5/12 h-5/6 backdrop-blur-xl bg-white/30 p-8 rounded-lg drop-shadow-2xl">
+                <div className='mb-4'>
+                    <Input
+                        className='flex-1 drop-shadow-xl'
+                        placeholder='Search to-do items...'
+                        size='lg'
+                        //value={text}
+                        //onChange={(e) => setText(e.target.value)}
+                        startDecorator={<SearchIcon sx={{ color: 'text.tertiary' }} />}
+                        sx={{ borderRadius: '4rem' }}
+                    />
+                </div>
+                <Divider />
+                <div className='mb-4'></div>
                 <AddItemFieldBar />
                 <div className='mt-4'>
                     <ToDoList />
