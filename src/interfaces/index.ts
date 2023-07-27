e2ed1d63605e11c2c6c9a5ad3dc28c4b45614605
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-27 14:12:56 -0400

    Creating new interfaces and enums

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,15 @@
+import {
+    Sort,
+} from 'src/static/enums'
+
 export type IToDoItem = {
     id: string,
     text: string,
     createdAt: Date,
     completed: boolean
+}
+
+export type ISortByItem = {
+    value: Sort,
+    label: string
 }
---
+++ src/static/enums.ts
@@ -0,0 +1,24 @@
+export enum Sort {
+    Incmpl1st = 'INCMPL_1ST',
+    Cmpl1st = 'CMPL_1ST',
+    DateAsc = 'DATE_ASC',
+    DateDesc = 'DATE_DESC',
+    TextAsc = 'TEXT_ASC',
+    TextDesc = 'TEXT_DESC',
+}
+
+export enum JoyUIColor {
+    Primary = 'primary',
+    Success = 'success',
+    Warning = 'warning',
+    Neutral = 'neutral',
+}
+
+export enum JoyUIVariant {
+    Solid = 'solid',
+    Soft = 'soft',
+}
+
+export enum JoyUISize {
+    Small = 'sm'
+}
