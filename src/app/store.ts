72f538e63da2fa0e0d9291f406fa3f6db437aab3
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-28 11:05:56 -0400

    Creating more hooks and type for store

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,17 @@ import { configureStore } from '@reduxjs/toolkit'
 import todoItemsReducer from 'src/features/todoItems/todoItemsSlice'
 import todoItemsFiltersReducer from 'src/features/todoItemsFilters/todoItemsFiltersSlice'
 
-export const store = configureStore({
-    reducer: {
-        todoItems: todoItemsReducer,
-        todoItemsFilters: todoItemsFiltersReducer,
-    }
-});
+export const createStore = () => {
+    return configureStore({
+        reducer: {
+            todoItems: todoItemsReducer,
+            todoItemsFilters: todoItemsFiltersReducer,
+        }
+    });
+}
 
+export const store = createStore();
+
+export type IStoreType = typeof store;
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
