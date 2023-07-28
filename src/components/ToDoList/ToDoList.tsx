d4b074647b8d29724bed850b72f0192e43a1254a
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-27 23:00:17 -0400

    Adding messages when items are completed or not found

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -4,8 +4,13 @@ import { Item } from 'src/components'
 import { isOlderItem } from 'src/utils';
 
 function ToDoList() {
+    const NO_ITEMS_MATCH_MSG = 'No to-do items found that meet the current search and filter criteria.';
+    const ITEMS_COMPLETED_MSG = 'Hooray! You completed all of your to-do items. 🎉';
+
     const todoItems = useAppSelector(state => state.todoItems);
     const todoItemsFilters = useAppSelector(state => state.todoItemsFilters);
+
+    const todoItemsExist = todoItems.length > 0;
 
     const itemMeetsCriteria = (item: IToDoItem) => {
         const includesSearchTeam = todoItemsFilters.searchTerm === '' ? true : item.text.includes(todoItemsFilters.searchTerm);
@@ -15,11 +20,17 @@ function ToDoList() {
             (todoItemsFilters.showCompleted && item.completed)) && includesSearchTeam;
     }
 
+    const filteredItems = todoItems.filter(item => itemMeetsCriteria(item));
+    const filteredItemsExist = filteredItems.length > 0;
+
     return (
-        // JLA - Work on what happens with multiple elements
         <div className="grid grid-flow-row row-span-1 overflow-auto">
             {
-                todoItems.map((item: IToDoItem) => itemMeetsCriteria(item) ? <div key={item.id} className='mb-4'><Item item={item} /></div> : <></>)
+                todoItemsExist ?
+                    filteredItemsExist ?
+                        filteredItems.map((item: IToDoItem) => <div key={item.id} className='mb-4'><Item item={item} /></div>) :
+                        <span className='text-center mt-4'>{NO_ITEMS_MATCH_MSG}</span>
+                    : <span className='text-center mt-4'>{ITEMS_COMPLETED_MSG}</span>
             }
         </div>
     );
