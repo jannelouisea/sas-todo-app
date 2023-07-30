5ee816cc198a67dffd35206fe08f144b72f519f7
Janne Louise Ave <janne.louise.a@gmail.com>
2023-07-30 16:52:07 -0400

    Cleaning up ToDoApp styles, adding custom title and favicon.ico

--- src/components/ToDoApp/ToDoApp.tsx
+++ src/components/ToDoApp/ToDoApp.tsx
@@ -9,8 +9,8 @@ import Divider from '@mui/joy/Divider';
 
 function ToDoApp() {
     return (
-        <div className="to-do-app w-screen h-screen flex justify-center items-center">
-            <div className="w-96 sm:w-9/12 md:w-9/12 lg:w-9/12 max-w-3xl h-5/6 backdrop-blur-xl bg-zinc-100/60 p-8 rounded-lg drop-shadow-2xl flex flex-col">
+        <div className="to-do-app">
+            <div className="to-do-app_content">
                 <div className='to-do-app_section'>
                     <SearchBar className='to-do-app_section' />
                     <div className='to-do-app_filter-and-sort'>
